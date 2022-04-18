8fe61364282fb6a92f856888e5c0d2476826a592
John Fahnestock <johnfahnestock@live.com>
2022-04-17 21:20:23 -0400

    Commented testing lines

--- ProgressPalService/src/main/java/com/trackerservice/ProgressPal/ProgressPalServiceApplication.java
+++ ProgressPalService/src/main/java/com/trackerservice/ProgressPal/ProgressPalServiceApplication.java
@@ -20,20 +20,20 @@ public class ProgressPalServiceApplication {
 		SpringApplication.run(ProgressPalServiceApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner run (UserService userService) {
-		return args -> {
-			userService.saveRole(new UserRole(null, "ROLE_USER"));
-			userService.saveRole(new UserRole(null, "ROLE_MANAGER"));
-			userService.saveRole(new UserRole(null, "ROLE_ADMIN"));
-			userService.saveRole(new UserRole(null, "ROLE_SUPER_ADMIN"));
-			userService.saveUser(new User("John", "Fahnestock", "jf@live.com", "password123", new ArrayList<>()));
-			userService.saveUser(new User("will", "Smith", "ws@live.com", "password123", new ArrayList<>()));
-
-			userService.addRoleToUser("jf@live.com", "ROLE_USER");
-			userService.addRoleToUser("ws@live.com", "ROLE_ADMIN");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run (UserService userService) {
+//		return args -> {
+//			userService.saveRole(new UserRole(null, "ROLE_USER"));
+//			userService.saveRole(new UserRole(null, "ROLE_MANAGER"));
+//			userService.saveRole(new UserRole(null, "ROLE_ADMIN"));
+//			userService.saveRole(new UserRole(null, "ROLE_SUPER_ADMIN"));
+//			userService.saveUser(new User("John", "Fahnestock", "jf@live.com", "password123", new ArrayList<>()));
+//			userService.saveUser(new User("will", "Smith", "ws@live.com", "password123", new ArrayList<>()));
+//
+//			userService.addRoleToUser("jf@live.com", "ROLE_USER");
+//			userService.addRoleToUser("ws@live.com", "ROLE_ADMIN");
+//		};
+//	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
