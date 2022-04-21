cad3979e99717d88036e7412f2408b0892e7ec77
John Fahnestock <johnfahnestock@live.com>
2022-04-21 14:57:54 -0400

    Altered cors for production on backend

--- ProgressPalService/src/main/java/com/trackerservice/ProgressPal/ProgressPalServiceApplication.java
+++ ProgressPalService/src/main/java/com/trackerservice/ProgressPal/ProgressPalServiceApplication.java
@@ -27,40 +27,40 @@ public class ProgressPalServiceApplication {
 		SpringApplication.run(ProgressPalServiceApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner run (UserService userService, MovementService movementService, WorkoutService mWorkoutService) {
-//		return args -> {
-//			userService.saveRole(new UserRole(null, "ROLE_USER"));
-//			userService.saveRole(new UserRole(null, "ROLE_MANAGER"));
-//			userService.saveRole(new UserRole(null, "ROLE_ADMIN"));
-//			userService.saveRole(new UserRole(null, "ROLE_SUPER_ADMIN"));
-//			userService.saveUser(new User("John", "Fahnestock", "jf@live.com", "password123", new ArrayList<>()));
-//			userService.saveUser(new User("will", "Smith", "ws@live.com", "password123", new ArrayList<>()));
-//
-//			userService.addRoleToUser("jf@live.com", "ROLE_USER");
-//			userService.addRoleToUser("ws@live.com", "ROLE_ADMIN");
-//
-//			mWorkoutService.addNewWorkout(new Workout("sdds1", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds2", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds3", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds4", LocalDate.now(), 1, "sdf", true, 2));
-//			mWorkoutService.addNewWorkout(new Workout("sdds5", LocalDate.now(), 1, "sdf", true, 2));
-//			mWorkoutService.addNewWorkout(new Workout("sdds6", LocalDate.now(), 1, "sdf", true, 2));
-//			mWorkoutService.addNewWorkout(new Workout("sdds7", LocalDate.of(2022,4,18), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds8", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds9", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds10", LocalDate.now(), 1, "sdf", true, 1));
-//			mWorkoutService.addNewWorkout(new Workout("sdds11", LocalDate.now(), 1, "sdf", true, 1));
-//
-//			movementService.addNewMovementForUser(new Movement("Fun", 2, 2, 2, 2, 1), 1);
-//			movementService.addNewMovementForUser(new Movement("Fun1", 2, 2, 2, 2, 1), 1);
-//			movementService.addNewMovementForUser(new Movement("Fun2", 2, 2, 2, 2, 1), 1);
-//
-//			movementService.addNewMovementForUser(new Movement("Fun", 2, 2, 2, 2, 2), 2);
-//			movementService.addNewMovementForUser(new Movement("Fun1", 2, 2, 2, 2, 2), 2);
-//			movementService.addNewMovementForUser(new Movement("Fun2", 2, 2, 2, 2, 2), 2);
-//		};
-//	}
+	@Bean
+	CommandLineRunner run (UserService userService, MovementService movementService, WorkoutService mWorkoutService) {
+		return args -> {
+			userService.saveRole(new UserRole(null, "ROLE_USER"));
+			userService.saveRole(new UserRole(null, "ROLE_MANAGER"));
+			userService.saveRole(new UserRole(null, "ROLE_ADMIN"));
+			userService.saveRole(new UserRole(null, "ROLE_SUPER_ADMIN"));
+			userService.saveUser(new User("John", "Fahnestock", "jf@live.com", "password123", new ArrayList<>()));
+			userService.saveUser(new User("will", "Smith", "ws@live.com", "password123", new ArrayList<>()));
+
+			userService.addRoleToUser("jf@live.com", "ROLE_USER");
+			userService.addRoleToUser("ws@live.com", "ROLE_ADMIN");
+
+			mWorkoutService.addNewWorkout(new Workout("sdds1", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds2", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds3", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds4", LocalDate.now(), 1, "sdf", true, 2));
+			mWorkoutService.addNewWorkout(new Workout("sdds5", LocalDate.now(), 1, "sdf", true, 2));
+			mWorkoutService.addNewWorkout(new Workout("sdds6", LocalDate.now(), 1, "sdf", true, 2));
+			mWorkoutService.addNewWorkout(new Workout("sdds7", LocalDate.of(2022,4,18), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds8", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds9", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds10", LocalDate.now(), 1, "sdf", true, 1));
+			mWorkoutService.addNewWorkout(new Workout("sdds11", LocalDate.now(), 1, "sdf", true, 1));
+
+			movementService.addNewMovementForUser(new Movement("Fun", 2, 2, 2, 2, 1), 1);
+			movementService.addNewMovementForUser(new Movement("Fun1", 2, 2, 2, 2, 1), 1);
+			movementService.addNewMovementForUser(new Movement("Fun2", 2, 2, 2, 2, 1), 1);
+
+			movementService.addNewMovementForUser(new Movement("Fun", 2, 2, 2, 2, 2), 2);
+			movementService.addNewMovementForUser(new Movement("Fun1", 2, 2, 2, 2, 2), 2);
+			movementService.addNewMovementForUser(new Movement("Fun2", 2, 2, 2, 2, 2), 2);
+		};
+	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -69,7 +69,10 @@ public class ProgressPalServiceApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				// Updated for production: https://progress-pal-front.herokuapp.com
 				// Local: http://localhost:3000
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("https://progress-pal-front.herokuapp.com");
+				//registry.addMapping("/**").allowedOrigins("http://progress-pal-front.herokuapp.com");
+				//registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				//registry.addMapping("/**").allowedOrigins("http://www.progresspal.xyz");
 			}
 		};
 	}
